dcb981e774c03bc7c171c051f3c0d77cf329ff85
Christophe Avenel <6164758+cavenel@users.noreply.github.com>
2020-09-01 15:14:37 +0200

    Update flasktissuumaps.py

--- flasktissuumaps.py
+++ flasktissuumaps.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python
 #
-# deepzoom_multiserver - Example web application for viewing multiple slides
-#
-# Copyright (c) 2010-2015 Carnegie Mellon University
+# flaskTissUUmaps - a minimal python server for TissUUmaps using Flask
 #
 # This library is free software; you can redistribute it and/or modify it
-# under the terms of version 2.1 of the GNU Lesser General Public License
+# under the terms of version 3.0 of the GNU General Public License
 # as published by the Free Software Foundation.
 #
 # This library is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
 # License for more details.
 #
-# You should have received a copy of the GNU Lesser General Public License
+# You should have received a copy of the GNU General Public License
 # along with this library; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
