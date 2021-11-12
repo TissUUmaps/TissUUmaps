df407bf7f7f43b8c9e8ad501956d8e110b79b288
Christophe Avenel <6164758+cavenel@users.noreply.github.com>
2021-11-12 12:01:00 +0100

    Move to 3.0.1.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='TissUUmaps',  
-     version='3.0.1.4',
+     version='3.0.1.5',
      author="Leslie Solorzano, Christophe Avenel, Fredrik Nysjö",
      author_email="christophe.avenel@it.uu.se",
      description="TissUUmaps is a lightweight viewer that uses basic web tools to visualize gene expression data or any kind of point data on top of whole slide images.",
