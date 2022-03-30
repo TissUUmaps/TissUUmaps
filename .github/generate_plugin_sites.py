3271c54782134efc7dd8747db9c3eaa4c5d8374c
Christophe Avenel <6164758+cavenel@users.noreply.github.com>
2022-03-30 16:50:25 +0200

    Update generate_plugin_sites.py

--- .github/generate_plugin_sites.py
+++ .github/generate_plugin_sites.py
@@ -1,5 +1,6 @@
 import glob
 from jinja2 import Template
+import yaml
 import os
 import json
 
@@ -14,6 +15,9 @@ t = Template("""
                 width: 100%; 
                 border-collapse: 
                 collapse; border-spacing: 0; 
+                max-width:1600px;
+                margin-left:auto;
+                margin-right:auto;
             }
 
             td, th { border: 1px solid #CCC; height: 30px; } /* Make cells a bit taller */
@@ -30,29 +34,31 @@ t = Template("""
                 padding: 10px;
             }
             td.thumb {
-                width: 20%;
+                width: 33%;
                 white-space: nowrap;
             }
         </style>
     </head>
     <body>
         <h1>TissUUmaps plugins</h1>
+        <table>
         {% for plugin in pluginList %}\
-            <h2>{{plugin.name}}:</h2>
-            <table>
                 <tr>
-                    {% if plugin.img %}\
-                        <td class="thumb">
-                            <img src="{{plugin.img}}" style="max-height:500px;width:100%;"/><br/>
-                        </td>
-                    {% endif %}
                     <td>
-                        {% if plugin.txt %}\
+                        <h2>{{plugin.name}}:</h2>
+                        {% if plugin.author %}\
+                            <p><i>Author: {{plugin.author}} </i></p>
+                        {% endif %}
+                        {% if plugin.version %}\
+                            <p><i>Version: {{plugin.version}}</i></p>
+                        {% endif %}
+                        {% if plugin.description %}\
                             <h3>Description</h3>
-                            {{plugin.txt}}
+                            {{plugin.description}}
                         {% endif %}
                         <h3>Plugin Files</h3>
                         <ul>
+                            <li><a href="{{plugin.yml}}">{{plugin.yml}}</a></li>
                             {% if plugin.py %}
                                 <li><a href="{{plugin.py}}">{{plugin.py}}</a></li>
                             {% endif %}
@@ -61,10 +67,14 @@ t = Template("""
                             {% endif %}
                         </ul>
                     </td>
+                    <td class="thumb">
+                        {% if plugin.image %}\
+                            <img src="{{plugin.image}}" style="max-height:500px;width:100%;"/><br/>
+                        {% endif %}
+                    </td>
                 </tr>
-            </table>
-            <hr/>
         {% endfor %}\
+        </table>
         <h2>Files needed by TissUUmaps for plugin installation:</h2>
         <ul><li><a href="pluginList.json">pluginList.json</a></li></ul>
         <table>
@@ -72,29 +82,31 @@ t = Template("""
 </html>
 """)
 
+def alphaSort (key):
+    if ("Plugin_template" in key):
+        return "ZZZ"
+    return key
+
 pluginList = []
-for pythonFile in glob.glob(r"../tissuumaps/plugins_available/*.py"):
-    print (pythonFile)
-    pluginObject = {
-        "py":os.path.basename(pythonFile), 
-        "name":os.path.basename(pythonFile).replace(".py","").replace("_"," ")
-    }
-    
-    for imgFmt in [".png",".gif",".jpg",".jpeg"]:
-        imgFile = pythonFile.replace(".py",imgFmt)
-        if os.path.isfile(pythonFile.replace(".py",imgFmt)):
-            pluginObject["img"] = os.path.basename(imgFile)
-    txtFile = pythonFile.replace(".py",".txt")
-    if os.path.isfile(txtFile):
-        with open(txtFile) as f:
-            pluginObject["txt"] = f.read()
-    jsFile = pythonFile.replace(".py",".js")
-    if os.path.isfile(jsFile):
-        pluginObject["js"] = os.path.basename(jsFile)
-    pluginList.append(pluginObject)
+ymlFiles = glob.glob(r"../tissuumaps/plugins_available/*.yml")
+ymlFiles.sort(key=alphaSort)
+for ymlFile in ymlFiles:
+    try:
+        with open(ymlFile, 'r') as stream:
+            pluginObject = yaml.safe_load(stream)
+        pluginObject["yml"] = os.path.basename(ymlFile)
+        pyFile = ymlFile.replace(".yml",".py")
+        if os.path.isfile(pyFile):
+            pluginObject["py"] = os.path.basename(pyFile)
+        jsFile = ymlFile.replace(".yml",".js")
+        if os.path.isfile(jsFile):
+            pluginObject["js"] = os.path.basename(jsFile)
+        pluginList.append(pluginObject)
+    except:
+        pass
 
 with open("../tissuumaps/plugins_available/pluginList.json","w") as f:
-    json.dump(pluginList, f, indent=4)
+    json.dump(pluginList, f, indent=4, sort_keys=True, default=str)
 
 with open("../tissuumaps/plugins_available/index.html","w") as f:
     f.write(t.render(pluginList=pluginList))
