0cf385b9d86df727d701baf3d0dfae17b90312f0
Christophe Avenel <6164758+cavenel@users.noreply.github.com>
2022-06-14 09:44:44 +0200

    Display index.html by default if present in main directory in read only mode

--- tissuumaps/views.py
+++ tissuumaps/views.py
@@ -331,6 +331,11 @@ def _get_slide(path, originalPath=None):
 @app.route("/")
 @requires_auth
 def index():
+    indexPath = os.path.abspath(os.path.join(app.basedir, "index.html"))
+    if os.path.isfile(indexPath) and app.config["READ_ONLY"]:
+        directory = os.path.dirname(indexPath)
+        filename = os.path.basename(indexPath)
+        return send_from_directory(directory, filename)
     return render_template(
         "tissuumaps.html",
         isStandalone=app.config["isStandalone"],
