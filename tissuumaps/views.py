1bcf4edd110bb3b0305867e6fc73c8e71430ec93
Christophe Avenel <christophe.avenel@it.uu.se>
2022-12-04 11:08:23 +0100

    Add range header and partial send in flask for h5 files

--- tissuumaps/views.py
+++ tissuumaps/views.py
@@ -8,6 +8,7 @@ import importlib
 import io
 import json
 import logging
+import mimetypes
 import os
 import pathlib
 import re
@@ -32,6 +33,7 @@ from flask import (
     redirect,
     render_template,
     request,
+    send_file,
     send_from_directory,
     url_for,
 )
@@ -590,7 +592,61 @@ def tile_asso(path, associated_name, level, col, row, format):
     return resp
 
 
-@app.route("/<path:filename>.<any(h5ad, adata):ext>")
+def send_file_partial(path):
+    """
+    Simple wrapper around send_file which handles HTTP 206 Partial Content
+    (byte ranges)
+    TODO: handle all send_file args, mirror send_file's error handling
+    (if it has any)
+    """
+    range_header = request.headers.get("Range", None)
+    if not range_header:
+        from flask import make_response, send_file
+
+        response = make_response(send_file(path))
+        response.headers["Accept-Ranges"] = "bytes"
+        return response
+
+    size = os.path.getsize(path)
+    byte1, byte2 = 0, None
+    print("Getting range:", range_header)
+    m = re.search("(\d+)-(\d*)", range_header)
+    g = m.groups()
+
+    if g[0]:
+        byte1 = int(g[0])
+    if g[1]:
+        byte2 = int(g[1])
+
+    length = size - byte1
+    if byte2 is not None:
+        length = byte2 - byte1
+
+    data = None
+    with open(path, "rb") as f:
+        f.seek(byte1)
+        data = f.read(length + 1)
+
+    rv = Response(
+        data, 206, mimetype=mimetypes.guess_type(path)[0], direct_passthrough=True
+    )
+    rv.headers["Accept-Ranges"] = "bytes"
+    # rv.headers.add('Content-Range', 'bytes {0}-{1}/{2}'.format(byte1, byte1 + length - 1, size))
+    rv.headers.add(
+        "Content-Range", "bytes {0}-{1}/{2}".format(byte1, byte1 + length, size)
+    )
+
+    # print ("Sent!", range_header, size, 'bytes {0}-{1}/{2}'.format(byte1, byte1 + length - 1, size))
+    print(
+        "Sent!",
+        range_header,
+        size,
+        "bytes {0}-{1}/{2}".format(byte1, byte1 + length, size),
+    )
+    return rv
+
+
+@app.route("/<path:filename>.<any(h5ad, adata, h5):ext>")
 @requires_auth
 def h5ad(filename, ext):
     path = request.args.get("path")
@@ -602,6 +658,10 @@ def h5ad(filename, ext):
     # Check if a .h5ad file exists:
     if not os.path.isfile(completePath):
         abort(404)
+    if "Referer" in request.headers.keys():
+        if "h5Utils_worker.js" in request.headers["Referer"]:
+            return send_file_partial(completePath)
+
     state = read_h5ad.h5ad_to_tmap(
         app.basedir, os.path.join(path, filename) + "." + ext
     )
