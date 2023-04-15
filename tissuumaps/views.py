9f50d809a0901793605ba71aba11519bed2baf37
Christophe Avenel <christophe.avenel@it.uu.se>
2023-04-15 22:49:16 +0200

    Fix bundleHTMLtoOneFile when not in local directory

--- tissuumaps/views.py
+++ tissuumaps/views.py
@@ -754,13 +754,16 @@ def bundleHTMLtoOneFile(htmlFile):
 
     from bs4 import BeautifulSoup
 
+    dirname = os.path.dirname(htmlFile)
     original_html_text = Path(htmlFile).read_text(encoding="utf-8")
     soup = BeautifulSoup(original_html_text, "html.parser")
 
     # Find link tags. example: <link rel="stylesheet" href="css/somestyle.css">
     for tag in soup.find_all("link", href=True):
         if tag.has_attr("href"):
-            file_text = Path(tag["href"]).read_text(encoding="utf-8")
+            file_text = Path(os.path.join(dirname, tag["href"])).read_text(
+                encoding="utf-8"
+            )
 
             # remove the tag from soup
             tag.extract()
@@ -773,7 +776,9 @@ def bundleHTMLtoOneFile(htmlFile):
     # Find script tags. example: <script src="js/somescript.js"></script>
     for tag in soup.find_all("script", src=True):
         if tag.has_attr("src"):
-            file_text = Path(tag["src"]).read_text(encoding="utf-8")
+            file_text = Path(os.path.join(dirname, tag["src"])).read_text(
+                encoding="utf-8"
+            )
 
             # remove the tag from soup
             tag.extract()
@@ -786,7 +791,7 @@ def bundleHTMLtoOneFile(htmlFile):
     # Find image tags.
     for tag in soup.find_all("img", src=True):
         if tag.has_attr("src"):
-            file_content = Path(tag["src"]).read_bytes()
+            file_content = Path(os.path.join(dirname, tag["src"])).read_bytes()
 
             # replace filename with base64 of the content of the file
             base64_file_content = base64.b64encode(file_content)
