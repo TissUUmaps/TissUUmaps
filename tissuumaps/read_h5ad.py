ec914f65048eb4aac0dcbd7d59012ce3dc3cbe98
Christophe Avenel <christophe.avenel@it.uu.se>
2023-04-24 09:01:32 +0200

    Clean unused vips function

--- tissuumaps/read_h5ad.py
+++ tissuumaps/read_h5ad.py
@@ -8,32 +8,6 @@ import numpy as np
 import pyvips
 from scipy import sparse
 
-
-def numpy2vips(a):
-    dtype_to_format = {
-        "uint8": "uchar",
-        "int8": "char",
-        "uint16": "ushort",
-        "int16": "short",
-        "uint32": "uint",
-        "int32": "int",
-        "float32": "float",
-        "float64": "double",
-        "complex64": "complex",
-        "complex128": "dpcomplex",
-    }
-    try:
-        height, width, bands = a.shape
-    except:
-        height, width = a.shape
-        bands = 1
-    linear = a.reshape(width * height * bands)
-    vi = pyvips.Image.new_from_memory(
-        linear.data, width, height, bands, dtype_to_format[str(a.dtype)]
-    )
-    return vi
-
-
 tmap_template = {
     "layers": [{"name": "tissue.tif", "tileSource": "./img/tissue.tif.dzi"}],
     "markerFiles": [],
