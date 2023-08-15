9b11efde65fb2a93093b98d7811512755a562bb6
Fredrik Nysjö <fredrik.nysjo@it.uu.se>
2023-08-15 15:32:14 +0200

    Fix darkening of filled markers with thick outlines

--- tissuumaps/static/js/utils/glUtils.js
+++ tissuumaps/static/js/utils/glUtils.js
@@ -258,7 +258,7 @@ glUtils._markersFS = `
         float distBias = u_markerFilled ? -pixelWidth * 0.25 : 0.0;  // Minification distance bias
 
         float distShape = (texture(u_shapeAtlas, uv, -2.0).r - 0.5) * 255.0 + distBias;
-        float distOutline = markerStrokeWidth - abs(distShape);
+        float distOutline = markerStrokeWidth - abs(distShape) + distBias;  // Add extra bias to fix darkening
         float alpha = clamp(distShape / pixelWidth + 0.5, 0.0, 1.0) * float(u_markerFilled);
         float alpha2 = clamp(distOutline / pixelWidth + 0.5, 0.0, 1.0) * float(u_markerOutline);
         if (distOutline < (markerStrokeWidth + 4.0) - 127.5) {
