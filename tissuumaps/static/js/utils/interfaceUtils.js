6956caa98fe09e96d44b1cb3b4d77d6771ba0020
Christophe Avenel <christophe.avenel@it.uu.se>
2023-08-31 21:47:30 +0200

    Fix filtering of markers based on row inner text
    Only use values inside label instead of the whole row for filtering markers

--- tissuumaps/static/js/utils/interfaceUtils.js
+++ tissuumaps/static/js/utils/interfaceUtils.js
@@ -2201,7 +2201,7 @@ interfaceUtils._mGenUIFuncs.groupUI=async function(uid, force){
         const trs = table.querySelectorAll('tbody tr')
         const filter = this.value
         const regex = new RegExp(filter, 'i')
-        const isFoundInTds = td => regex.test(td.innerText)
+        const isFoundInTds = td => regex.test(td.querySelector('label')?.innerText??"")
         const isFound = childrenArr => childrenArr.some(isFoundInTds)
         const setTrStyleDisplay = (element) => {
             if (isFound([
