6ed40425adc4d127e4ea965f5ca29bbc2c36e742
Fredrik Nysjö <fredrik.nysjo@it.uu.se>
2023-09-06 17:31:48 +0200

    Increase size of region LUT texture
    This increases the maximum number of unique region objects we can render to 524288.

--- tissuumaps/static/js/utils/glUtils.js
+++ tissuumaps/static/js/utils/glUtils.js
@@ -1629,7 +1629,7 @@ glUtils._createRegionLUTTexture = function(gl) {
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texStorage2D(gl.TEXTURE_2D, 1, gl.RGBA8, 4096, 64);
+    gl.texStorage2D(gl.TEXTURE_2D, 1, gl.RGBA8, 4096, 128);
     gl.bindTexture(gl.TEXTURE_2D, null);
 
     return texture;
@@ -1637,7 +1637,7 @@ glUtils._createRegionLUTTexture = function(gl) {
 
 
 glUtils._updateRegionLUTTexture = function(gl, texture) {
-    let texeldata = new Uint8Array((4096 * 64) * 4);
+    let texeldata = new Uint8Array((4096 * 128) * 4);
     if (regionUtils._regionToColorLUT.length <= texeldata.length) {
         texeldata.set(regionUtils._regionToColorLUT);
     } else {
@@ -1645,7 +1645,7 @@ glUtils._updateRegionLUTTexture = function(gl, texture) {
     }
 
     gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, 4096, 64, gl.RGBA, gl.UNSIGNED_BYTE, texeldata);
+    gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, 4096, 128, gl.RGBA, gl.UNSIGNED_BYTE, texeldata);
     gl.bindTexture(gl.TEXTURE_2D, null);
 }
 
