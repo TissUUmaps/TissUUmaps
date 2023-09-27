b72d5f181f7d433e0bf0ceb090c64768354aaa83
Christophe Avenel <christophe.avenel@it.uu.se>
2023-09-27 16:48:21 +0200

    Move from PyQt6 to PySide6

--- tissuumaps/gui.py
+++ tissuumaps/gui.py
@@ -9,13 +9,18 @@ debug_logger.flush = lambda: None  # this may be called when printing
 sys.stdout = debug_logger
 
 try:
-    from PyQt6 import QtGui
-    from PyQt6.QtCore import *
-    from PyQt6.QtGui import QAction, QDesktopServices, QStandardItem, QStandardItemModel
-    from PyQt6.QtWebChannel import QWebChannel
-    from PyQt6.QtWebEngineCore import *
-    from PyQt6.QtWebEngineWidgets import *
-    from PyQt6.QtWidgets import (
+    from PySide6 import QtGui
+    from PySide6.QtCore import *
+    from PySide6.QtGui import (
+        QAction,
+        QDesktopServices,
+        QStandardItem,
+        QStandardItemModel,
+    )
+    from PySide6.QtWebChannel import QWebChannel
+    from PySide6.QtWebEngineCore import *
+    from PySide6.QtWebEngineWidgets import *
+    from PySide6.QtWidgets import (
         QApplication,
         QDialog,
         QDialogButtonBox,
@@ -594,7 +599,7 @@ class webEngine(QWebEngineView):
         else:
             self.load(QUrl(self.location))
 
-    @pyqtSlot(str)
+    @Slot(str)
     def getProperties(self, path):
         try:
             path = urllib.parse.unquote(path)[:-4]
@@ -617,7 +622,7 @@ class webEngine(QWebEngineView):
         self.load(QUrl(self.location))
         self.mainWin.setWindowTitle("TissUUmaps")
 
-    @pyqtSlot(str, result="QJsonObject")
+    @Slot(str, result="QJsonObject")
     def exportToStatic(self, state):
         try:
             parsed_url = urlparse(self.url().toString())
@@ -637,7 +642,7 @@ class webEngine(QWebEngineView):
         except:
             return {"success": False, "error": traceback.format_exc()}
 
-    @pyqtSlot(str)
+    @Slot(str)
     def saveProject(self, state):
         def getRel(previouspath, file, newpath):
             completepath = os.path.dirname(os.path.join(previouspath, file))
@@ -742,11 +747,11 @@ class webEngine(QWebEngineView):
         self.mainWin.setWindowTitle("TissUUmaps - " + os.path.basename(folderpath))
         return True
 
-    @pyqtSlot()
+    @Slot()
     def exit(self):
         self.close()
 
-    @pyqtSlot(str, str, result="QJsonObject")
+    @Slot(str, str, result="QJsonObject")
     def addCSV(self, path, csvpath):
         if csvpath == "":
             csvpath = QFileDialog.getOpenFileName(self, "Select a File")[0]
@@ -785,7 +790,7 @@ class webEngine(QWebEngineView):
         }
         return returnDict
 
-    @pyqtSlot(str, str, result="QJsonObject")
+    @Slot(str, str, result="QJsonObject")
     def addLayer(self, path, layerpath):
         if layerpath == "":
             layerpath = QFileDialog.getOpenFileName(self, "Select a File")[0]
