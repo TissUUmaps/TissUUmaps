34fa1f2c39cf498c7180c34101e8fe61aa438ba7
Christophe Avenel <christophe.avenel@it.uu.se>
2023-11-06 22:17:47 +0100

    Fix local path for manual win installer

--- standalone/build.ps1
+++ standalone/build.ps1
@@ -1,3 +1,3 @@
 pyinstaller.exe .\TissUUmaps.spec --noconfirm
-Copy-Item -Path "C:\Program Files\vips-dev-8.10\bin\*" -Destination "dist\TissUUmaps\" -Recurse
+Copy-Item -Path "dep\vips-dev-8.10\bin\*" -Destination "dist\TissUUmaps\" -Recurse
 ISCC.exe build_installer.iss
