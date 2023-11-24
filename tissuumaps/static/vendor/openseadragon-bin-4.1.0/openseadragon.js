d20bdda8480fdc6c0aef7be8adb25f2bea3956e3
Christophe Avenel <christophe.avenel@it.uu.se>
2023-11-24 14:53:47 +0100

    Fix unwanted smoothing at high magnification
    Fix OpenSeadragon unwanted smoothing of sketch context

--- tissuumaps/static/vendor/openseadragon-bin-4.1.0/openseadragon.js
+++ tissuumaps/static/vendor/openseadragon-bin-4.1.0/openseadragon.js
@@ -19877,6 +19877,9 @@ $.Drawer.prototype = {
         if ( this.useCanvas ) {
             this._imageSmoothingEnabled = imageSmoothingEnabled;
             this._updateImageSmoothingEnabled(this.context);
+            if (this.sketchContext) {
+                this._updateImageSmoothingEnabled(this.sketchContext);
+            }
             this.viewer.forceRedraw();
         }
     },
